80354782f00af5177d05020383d019c605407e22
koen-benne <koen.benne@gmail.com>
2022-10-27 15:27:38 +0200

    some configs for neovide ahyeah

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -35,6 +35,14 @@ for k, v in pairs(options) do
     vim.opt[k] = v
 end
 
+if vim.g.neovide then
+  vim.g.neovide_transparency = 0.8
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_cursor_animation_length = 0.01
+end
+
 -- Autocommands
 --[[ vim.cmd [[ ]]
 --[[ augroup kitty_mp ]]
