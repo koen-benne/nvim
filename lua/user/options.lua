096f4b451441852d3e4c3d01a57d3d288fcbb0c5
koen-benne <koen.benne@gmail.com>
2022-10-28 13:51:26 +0200

    set font for gui apps

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,17 +1,17 @@
 local options = {
     backup = false,
     clipboard = "unnamedplus",
-    completeopt = { "menu", "menuone", "noselect" },		-- cmp stuff
-    mouse = "a",				                                -- allow mouse use
+    completeopt = { "menu", "menuone", "noselect" },	-- cmp stuff
+    mouse = "a",				                              -- allow mouse use
     smartcase = true,
     smartindent = true,
     number = true,
-    pumheight = 10,                                     -- pop up menu height
+    pumheight = 10,                                   -- pop up menu height
     incsearch = true,
     visualbell = true,
     showtabline = 2,
-    numberwidth = 2,                                    -- set number column width to 2 {default 4}
-    signcolumn = "yes",                                 -- always show the sign column to prevent shifting text
+    numberwidth = 2,                                  -- set number column width to 2 {default 4}
+    signcolumn = "yes",                               -- always show the sign column to prevent shifting text
     expandtab = true,
     shiftwidth = 2,
     tabstop = 2,
@@ -26,7 +26,8 @@ local options = {
     splitbelow = true,
     splitright = true,
     termguicolors = true,
-    laststatus = 2,                                     -- Single statusline when set to 3
+    laststatus = 2,                                   -- Single statusline when set to 3
+    guifont = "JetBrainsMonoNL Nerd Font Mono:h15",   -- font used in GUI neovim
 }
 
 vim.opt.shortmess:append "c"
@@ -44,8 +45,6 @@ if vim.g.neovide then
   vim.g.neovide_cursor_animation_length = 0.01
   vim.g.neovide_scale_factor = 1.0
   vim.g.neovide_scroll_animation_length = 0.6
-  --[[ vim.g.neovide_winblend = 90 ]]
-  --[[ vim.g.neovide_pumblend = 90 ]]
   vim.g.neovide_floating_opacity = 0.6
   function ChangeScaleFactor(delta)
       vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
