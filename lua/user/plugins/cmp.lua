ceb736fb1fe1abb9d767cf2854c4d1f4108ff6d2
koen-benne <koen.benne@gmail.com>
2022-11-28 17:52:28 +0100

    made cmp look nicer

--- lua/user/plugins/cmp.lua
+++ lua/user/plugins/cmp.lua
@@ -123,23 +123,24 @@ cmp.setup {
         vim_item.kind = icons.misc.CircuitBoard
         vim_item.kind_hl_group = "CmpItemKindConstant"
       end
+
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       -- NOTE: order matters
       vim_item.menu = ({
-        copilot = "[Copilot]",
-        nvim_lsp = "[LSP]",
-        nvim_lua = "[Nvim]",
-        luasnip = "[Snippet]",
-        buffer = "[Buffer]",
-        path = "[Path]",
-        emoji = "[Emoji]",
+        -- copilot = "[Copilot]",
+        -- nvim_lsp = "[LSP]",
+        -- nvim_lua = "[Nvim]",
+        -- luasnip = "[Snippet]",
+        -- buffer = "[Buffer]",
+        -- path = "[Path]",
+        -- emoji = "[Emoji]",
 
-        -- nvim_lsp = "",
-        -- nvim_lua = "",
-        -- luasnip = "",
-        -- buffer = "",
-        -- path = "",
-        -- emoji = "",
+        nvim_lsp = "",
+        nvim_lua = "",
+        luasnip = "",
+        buffer = "",
+        path = "",
+        emoji = "",
       })[entry.source.name]
       return vim_item
     end,
