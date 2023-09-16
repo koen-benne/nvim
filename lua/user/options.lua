27dc019df22e154f37b65600f0a2c4327d042811
koen-benne <koen.benne@gmail.com>
2023-09-16 15:08:11 +0200

    taa

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -40,7 +40,7 @@ for k, v in pairs(options) do
     vim.opt[k] = v
 end
 
--- Ufo stuff
+-- Folding stuff
 vim.o.foldcolumn = '0' -- '0' is not bad
 vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
