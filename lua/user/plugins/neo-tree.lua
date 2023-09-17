c4513584027af91467e1a36a253ffc77a97a59ca
koen-benne <koen.benne@gmail.com>
2023-09-17 12:26:51 +0200

    yuh

--- lua/user/plugins/neo-tree.lua
+++ lua/user/plugins/neo-tree.lua
@@ -5,19 +5,6 @@ end
 
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
--- If you want icons for diagnostic errors, you'll need to define them somewhere:
--- Something needs to be done about the guibg of these
--- vim.fn.sign_define("DiagnosticSignError",
---   {text = " ", texthl = "DiagnosticSignError"})
--- vim.fn.sign_define("DiagnosticSignWarn",
---   {text = " ", texthl = "DiagnosticSignWarn"})
--- vim.fn.sign_define("DiagnosticSignInfo",
---   {text = " ", texthl = "DiagnosticSignInfo"})
--- vim.fn.sign_define("DiagnosticSignHint",
---   {text = "", texthl = "DiagnosticSignHint"})
--- NOTE: this is changed from v1.x, which used the old style of highlight groups
--- in the form "LspDiagnosticsSignWarning"
-
 local no_border = { " ", " " }
 
 neo_tree.setup({
