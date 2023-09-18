21228b8f65357027e46a9684cfe53f5982a4b682
koen-benne <koen.benne@gmail.com>
2023-09-18 13:09:29 +0200

    gek

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 configs.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = "all",
+  -- ensure_installed = "all",
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
